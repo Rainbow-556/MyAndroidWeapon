ec9e66c0e5cbf6f4cc9cd5cc393c0f28f542aab3
glennli <glennli@lexin.com>
2019-06-26 19:30:30 +0800

    1.初始化工程

---
+++ module_common/src/main/java/com/lx/module/common/util/FileUtil.java
@@ -0,0 +1,7 @@
+package com.lx.module.common.util;
+
+/**
+ * Created by glennli on 2019/6/26.<br/>
+ */
+public final class FileUtil {
+}
---
+++ module_webViewCache/src/main/java/com/lx/module/webviewcache/WebActivity.java
@@ -0,0 +1,7 @@
+package com.lx.module.webviewcache;
+
+/**
+ * Created by glennli on 2019/6/26.<br/>
+ */
+public final class WebActivity {
+}
